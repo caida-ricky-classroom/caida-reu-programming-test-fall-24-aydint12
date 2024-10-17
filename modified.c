23b5ad9c1a9a08127cc4191f608c137ea49385ed
aydint12 <166767995+aydint12@users.noreply.github.com>
2024-10-16 17:24:16 -0700

    Fixed modified.c

--- modified.c
+++ modified.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-#include <linux/if_ether.h>
+// #include <linux/if_ether.h> // Removed because it's causing an error
+
+#define ETHERNET_HEADER_SIZE 14 // Ethernet headers are 14 bytes long
 
 int main(int argc, char *argv[]) {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
     const unsigned char *packet;
     struct pcap_pkthdr header;
-    struct iphdr *ip_header;
+    struct ip *ip_header; // Changed to struct ip
     int packet_count = 0;
 
     if (argc != 2) {
@@ -24,8 +26,11 @@ int main(int argc, char *argv[]) {
     }
 
     while ((packet = pcap_next(handle, &header)) != NULL) {
-        ip_header = (struct iphdr*)(packet + sizeof(struct ethhdr));
-        printf("Packet %d: IP destination address: %s\n", ++packet_count, inet_ntoa(*((struct in_addr*)ip_header->daddr)));
+        // Move past the Ethernet header to get to the IP header
+        ip_header = (struct ip*)(packet + ETHERNET_HEADER_SIZE); // Adjusted the offset and struct
+
+        // Print the destination IP address
+        printf("Packet %d: IP destination address: %s\n", ++packet_count, inet_ntoa(ip_header->ip_dst));
     }
 
     pcap_close(handle);
